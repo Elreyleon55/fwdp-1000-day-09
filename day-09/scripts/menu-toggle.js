4ad80e0c7e68bb2493541f55545db367c6c6ce6e
Benjamin <benjamin.4179@gmail.com>
2024-05-15 10:41:05 -0700

    added code for a mune toogle in javascript and added display none display block in css fpr whne lick it appers

--- day-09/scripts/menu-toggle.js
+++ day-09/scripts/menu-toggle.js
@@ -1,4 +1,11 @@
 // Main Navigation Toggle
+const button = document.getElementById("main-nav-button");
+const link = document.getElementById("main-nav-list");
 
+button.addEventListener("click",() => {
+  link.classList.toggle('toggled');
+  
+
+})
 
 // Thumb Navigation Toggle
